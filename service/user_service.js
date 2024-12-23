71e14f5ef86dcc5cac3a9a22685eb6b3e55471d9
kalpana-1 <kalpana.tamatam@arithaconsulting.com>
2024-12-23 15:36:49 +0530

    Generating user password while creating users and sending mail

--- service/user_service.js
+++ service/user_service.js
@@ -6,6 +6,19 @@ const insertUserService = async (params) => {
   try {
     let data = {};
     params.user_password = generateRandomPassword();
+
+    // const mailOptions = {
+    //   from: process.env.FROM,
+    //   to: params.user_email.toLowerCase(),
+    //   text: params.user_password,
+    //   subject: 'Your password for TIC application',
+    //   html: `<p>Hello, please enter the below password while you're logging in,</p>
+    //         <p><b>${params.user_password}</b></p>
+    //         <p>Note: This is an auto-generated email. Please do not reply to it.</p>
+    //         <p>Thanks,<br>Team TIC</p>`,
+    // };
+
+    // await smtpTransporter.sendMail(mailOptions);
     const res = await userQuery('CREATE_USER', params);
     let user_id = res?.insertId ? res.insertId : 0;
     if (user_id) {
